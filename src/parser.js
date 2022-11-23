848643fa4ba7cd536a5b7aec8cc3989925055cd0
Maelttt <mael.turchetto@orange.fr>
2022-11-23 14:15:17 +0100

    y'a moyen ça marche laureates insert

--- src/parser.js
+++ src/parser.js
@@ -6,27 +6,25 @@ const dataBuffer = fs.readFileSync("prize.json");
 let data = dataBuffer.toString();
 data = JSON.parse(data);
 
-async function insertLaureates(){
+async function insertLaureates() {
     await pool.query('TRUNCATE prizes,laureates,categories')
-    let laureats = [];
     const laureate_ids = [];
     for (let i = 0; i < data.length; i++) {
-        if(data[i].laureates){
-            for (let j = 0; j < data[i].laureates.length; j++) {
-                if(!laureate_ids.includes(data[i].laureates[j].id)){
-                    laureate_ids.push(data[i].laureates[j].id);
-                    laureats.push(data[i].laureates[j])
-                }
+        if (!data[i].laureates) { continue; }
+        for (let j = 0; j < data[i].laureates.length; j++) {
+            if (!laureate_ids.includes(data[i].laureates[j].id)) {
+                laureate_ids.push(data[i].laureates[j].id);
+
+                await pool.query('INSERT INTO laureates (id_laureate, firstname, surname) VALUES ($1,$2,$3)',
+                    [data[i].laureates[j].id, data[i].laureates[j].firstname, data[i].laureates[j].surname], (err) => {
+                        if(err) throw err;
+                    });
+
             }
         }
     }
-    for (let i = 0; i < laureats.length; i++) {
-        await pool.query('INSERT INTO laureates (id_laureate, firstname, surname) VALUES ($1,$2,$3)',
-            [laureats[i].id,laureats[i].firstname,laureats[i].surname], (err) => {
-            if(err) throw err;
-        })
-    }
 }
+
 
 async function insertCategories(){
 
