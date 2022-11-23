5f19fa3050a1083a40e25053e7411ee81cef6835
LucasFerryPro <lucas.ferry.ecole@gmail.com>
2022-11-23 13:52:52 +0100

    fuck parser

--- src/parser.js
+++ src/parser.js
@@ -2,24 +2,14 @@ const fs = require('fs');
 const pool = require('./db');
 
 //parse prize.json into postgres db
-const data = fs.readFileSync('prize.json');
-const json = JSON.parse(data.toString());
+const dataBuffer = fs.readFileSync("prize.json");
+let dataJSON = dataBuffer.toString();
+dataJSON = JSON.parse(dataJSON);
 
-for (let i = 0; i < json.length; i++) {
-    if(json[i].laureates) {
-        const l = json[i].laureates;
-        for (let j = 0; j < l.length; j++) {
-            pool.query('SELECT * FROM laureates WHERE id_laureate = $1', [l[j].id], (error, results) => {
-                if (error) throw error
-                if (results.rowCount === 0) {
-                    pool.query('INSERT INTO laureates (id_laureate, firstname, surname) VALUES ($1,$2,$3)',[l[j].id,l[j].firstname,l[j].surname], (err) => {
-                        if(err) console.log(err);
-                        else{
+async function insertLaureates(){
 
-                        }
-                    })
-                }
-            })
-        }
-    }
+}
+
+async function insertCategories(){
+
 }
