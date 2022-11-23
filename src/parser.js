05870bb4d1d73e6b05cb3c199320269f5bc88d31
LucasFerryPro <lucas.ferry.ecole@gmail.com>
2022-11-23 10:25:12 +0100

    add laureate parser

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,27 @@
-var fs = require('fs');
-var pool = require('./db');
+const fs = require('fs');
+const pool = require('./db');
 
 //parse prize.json into postgres db
-var data = fs.readFileSync('prize.json');
-var json = JSON.parse(data.toString());
+const data = fs.readFileSync('prize.json');
+const json = JSON.parse(data.toString());
 
+for (let i = 0; i < json.length; i++) {
+    if(json[i].laureates) {
+        const l = json[i].laureates;
+        for (let j = 0; j < l.length; j++) {
+            pool.query('SELECT * FROM laureates WHERE id_laureate = $1', [l[j].id], (error, results) => {
+                if (error) throw error
+                if (results.rowCount === 0) {
+                    pool.query('INSERT INTO laureates (id_laureate, firstname, surname) VALUES ($1,$2,$3)',[l[j].id,l[j].firstname,l[j].surname], (err, res) => {
+                        if(err) console.log(err);
+                        else {
+                            console.log(res.rows);
+                        }
+                    })
+                }
+            })
+        }
+    }
+}
 
 
