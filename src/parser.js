81a317691aa546b66feb8850c43bcc8fdaed0766
LucasFerryPro <lucas.ferry.ecole@gmail.com>
2022-11-23 14:50:51 +0100

    alelouya

--- src/parser.js
+++ src/parser.js
@@ -6,37 +6,41 @@ const dataBuffer = fs.readFileSync("prize.json");
 let data = dataBuffer.toString();
 data = JSON.parse(data);
 
-async function insertLaureates() {
+async function insert() {
+    let laureate_ids = [];
+    let categories = [];
     await pool.query('TRUNCATE prizes,laureates,categories')
-    const laureate_ids = [];
     for (let i = 0; i < data.length; i++) {
-        if (!data[i].laureates) { continue; }
-        for (let j = 0; j < data[i].laureates.length; j++) {
-            if (!laureate_ids.includes(data[i].laureates[j].id)) {
-                laureate_ids.push(data[i].laureates[j].id);
+        insertLaureate(data[i],laureate_ids);
 
+        insertCategorie(data[i],categories);
+    }
+}
+
+async function insertLaureate(data,laureate_ids){
+    if (data.laureates) {
+        for (let j = 0; j < data.laureates.length; j++) {
+            if (!laureate_ids.includes(data.laureates[j].id)) {
+                laureate_ids.push(data.laureates[j].id);
                 await pool.query('INSERT INTO laureates (id_laureate, firstname, surname) VALUES ($1,$2,$3)',
-                    [data[i].laureates[j].id, data[i].laureates[j].firstname, data[i].laureates[j].surname], (err) => {
+                    [data.laureates[j].id, data.laureates[j].firstname, data.laureates[j].surname], (err) => {
                         if(err) throw err;
-                    });
-
+                });
             }
         }
     }
 }
 
-
-async function insertCategories(){
-    const categories = [];
-    for (let i = 0; i < data.length; i++) {
-        if (!data[i].category) { continue; }
-        if (!categories.includes(data[i].label)) {
-            categories.push(data[i].label);
-            await pool.query('INSERT INTO Categories (label) VALUES ($1)', [data[i].label], (err) => {
-                if (err) throw err;
+async function insertCategorie(data,categories){
+    if (data.category) {
+        if (!categories.includes(data.category)) {
+            categories.push(data.category);
+            await pool.query('INSERT INTO categories (label) VALUES ($1)',
+                [data.category], (err) => {
+                    if (err) throw err;
             });
         }
     }
 }
 
-insertCategories();
+insert()
